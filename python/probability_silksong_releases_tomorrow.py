92f472e55979167b881e0c6d4ca6659b19e37c5a
jd-develop <jd-dev@laposte.net>
2023-03-25 23:59:45 +0100

    we all need silksong now

---
+++ python/probability_silksong_releases_tomorrow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+import datetime
+
+reveal_date = datetime.date(2019, 2, 14)
+today = datetime.date.today()
+june12 = datetime.date(2023, 6, 12)
+
+delta = (june12-today).days
+
+probability = 100/delta
+
+print(f"Cela fait {(today-reveal_date).days} jours que Hollow Knight: Silksong a été annoncé.")
+print(f"Il y a une chance sur {delta} que Hollow Knight: Silksong sorte demain, soit {probability}% de probabilité.")
+
---
+++ python/zip.py
@@ -0,0 +1,40 @@
+text = input("texte: ")
+
+hex_ = bytes(text, "UTF-8").hex()
+
+final_str = ""
+for loop in range(10):
+    # encode
+    final_str = ""
+    actual_hex = ""
+    for i in range(len(hex_)):
+        actual_hex += hex_[i]
+        if len(actual_hex) == 4:
+            char = chr(int(actual_hex, base=16))
+            final_str += char
+            actual_hex = ""
+
+    if actual_hex != "":
+        char = chr(int(actual_hex, base=16))
+        final_str += char
+
+    hex_ = bytes(final_str, "UTF-8").hex()
+
+    # print(len(text))
+    # print(len(final_str))
+    # print(final_str)
+
+print(final_str)
+
+input_decode = final_str
+output = ""
+for loop in range(10):
+    # decode
+    for char in input_decode:
+        ord_hex = hex(ord(char))[2:]
+        output += ord_hex
+    input_decode = bytes.fromhex(output).decode("UTF-8")
+    output = ""
+
+print(input_decode)
+# print(bytes.fromhex(input_decode).decode("UTF-8"))
