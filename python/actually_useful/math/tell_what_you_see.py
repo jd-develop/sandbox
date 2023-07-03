babff517411f04520624d270b1bb55b03bc52dd4
Jean Dubois <jd-dev@laposte.net>
2023-07-03 23:22:41 +0200

    add  some stuff

---
+++ python/actually_useful/math/tell_what_you_see.py
@@ -0,0 +1,23 @@
+import pprint
+
+l_ = ["1"]
+
+loop = int(input("How many terms should be calculated?\nIt starts being slow around ~40/50 and completely filling the "
+                 "Ubuntu terminal app at 45.\n>"))
+
+for i in range(loop):
+    a = l_[-1]
+    b = ""
+    enum_ = 0
+    current_num = a[0]
+    for c in a:
+        if c == current_num:
+            enum_ += 1
+        else:
+            b += str(enum_) + current_num
+            current_num = c
+            enum_ = 1
+    b += str(enum_) + current_num
+    l_.append(b)
+
+pprint.pprint(l_)
