36645d58c374431d36d71bd1ba42542f7ce5d97c
Jean Dubois <jd-dev@laposte.net>
2024-02-22 23:20:35 +0100

    Update philo.py
    ça me fume de rire

--- python/mess_and_misc/small_experiments/philo.py
+++ python/mess_and_misc/small_experiments/philo.py
@@ -10,15 +10,21 @@ def generate_sujet():
         ("liberté", 1, ["de penser", "d’opinion", "d’agir"]),
         ("recherche", 1, ["de soi", "de liberté", "de vérité"]),
         ("inconscient", 2, ["psychique"]),
-        ("vérité", 1, [])
+        ("vérité", 1, ["scientifique"]),
+        ("religion", 1, []),
+        ("travail", 0, ["selon Marx"]),
+        ("philosophie", 1, ["de Descartes"])
     ]
     end1 = [
         ("extension", True),
         ("partie", True),
-        ("état d’esprit", False)
+        ("état d’esprit", False),
+        ("selon vous assimilable", None)
     ]
     adjective = [
-        ("considéré", "considérée", ["comme une énigme", "comme une réflexion"]),
+        ("considéré", "considérée", ["comme une énigme", "comme une réflexion", "de façon métaphysique"]),
+        ("en tant que", "en tant que", ["valeur"]),
+        ("", "", [""])
     ]
     s = start.pop(random.randint(0, len(start)-1))
     e1 = random.choice(end1)
@@ -40,25 +46,39 @@ def generate_sujet():
     elif s[1] == 1 or s[1] == 3:
         result += "est-elle "
 
-    if e1[1]:
+    if e1[1] is None:
+        pass
+    elif e1[1]:
         result += "une "
     else:
         result += "un "
     result += e1[0] + " "
 
     if e2[1] == 0:
-        result += "du "
+        if e1[1] is None:
+            result += "au "
+        else:
+            result += "du "
     elif e2[1] == 1:
-        result += "de la "
+        if e1[1] is None:
+            result += "à la "
+        else:
+            result += "de la "
     else:
-        result += "de l’"
+        if e1[1] is None:
+            result += "à l’"
+        else:
+            result += "de l’"
     result += e2[0] + " "
     if len(e2[2]) != 0:
         if random.randint(0, 1) == 0:
             result += random.choice(e2[2]) + " "
 
-    result += e3[int(e2[1])] + " "
+    index_in_e3 = int(e2[1])%2
+    result += e3[index_in_e3] + " "
     result += random.choice(e3[2])
+    while result.endswith(" "):
+        result = result[:-1]
     result += "\N{NBSP}?"
 
     return result
