614b1098488aed2097e12805d226f01c87d8e5ed
Jean Dubois <jd-dev@laposte.net>
2024-08-13 22:28:10 +0100

    check my gitconfig

--- python/mess_and_misc/optimised_code.py
+++ python/mess_and_misc/optimised_code.py
@@ -1,7 +1,10 @@
-# coding: utf-8
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
 def call_func(func, *args, **kwargs):
     """ Best, easiest, most optimized and only way to call a function ^^ """
     func.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__ \
+        .__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__ \
         .__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__ \
         .__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__ \
         .__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__ \
