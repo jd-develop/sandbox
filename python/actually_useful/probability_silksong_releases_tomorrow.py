881f12b22272d3f30e9517e0d8c421d3a1cd915f
Jean Dubois <jd-dev@laposte.net>
2025-05-01 17:27:49 +0100

    silksooooong

--- python/actually_useful/probability_silksong_releases_tomorrow.py
+++ python/actually_useful/probability_silksong_releases_tomorrow.py
@@ -5,12 +5,17 @@ import datetime
 reveal_date = datetime.date(2019, 2, 14)
 today = datetime.date.today()
 june12 = datetime.date(2023, 6, 12)
-_2025 = datetime.date(2025, 12, 12)
+_2025 = datetime.date(2025, 12, 31)
+september18 = datetime.date(2025, 9, 18)
 
 delta = (_2025-today).days
+delta2 = (september18-today).days
 
 probability = 100/delta
+probability2 = 100/delta2
 
 print(f"Cela fait {(today-reveal_date).days} jours que Hollow Knight: Silksong a été annoncé.")
 print("Il a été annoncé que Hollow Knight: Silksong sortira en 2025, i.e. avant le 31 décembre 2025 inclus.")
 print(f"Il y a une chance sur {delta} que Hollow Knight: Silksong sorte demain, soit {probability}% de probabilité.")
+print("De plus, il est conjecturé que Hollow Knight: Silksong sortira avant le 18 septemble 2025 inclus.")
+print(f"Dans ce cas, il y a une chance sur {delta2} que Hollow Knight: Silksong sorte demain, soit {probability2}% de probabilité.")
