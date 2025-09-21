1b952b233ef609536d84a9c77e86880a0c4f860a
Jean Dubois <jd-dev@laposte.net>
2025-09-21 22:54:03 +0100

    btw silksong is out

--- python/actually_useful/probability_silksong_releases_tomorrow.py
+++ python/actually_useful/probability_silksong_releases_tomorrow.py
@@ -2,16 +2,17 @@
 # coding:utf-8
 import datetime
 
-reveal_date = datetime.date(2019, 2, 14)
+#reveal_date = datetime.date(2019, 2, 14)
 today = datetime.date.today()
-june12 = datetime.date(2023, 6, 12)
-september18 = datetime.date(2025, 9, 18)
+#june12 = datetime.date(2023, 6, 12)
+#september18 = datetime.date(2025, 9, 18)
 september4 = datetime.date(2025, 9, 4)
+#
+delta = (today-september4).days
+print(f"SILKSONG EST SORTI IL Y A {delta} JOURS")
+#
+#probability = int(delta == 1)
 
-delta = (september4-today).days
-
-probability = int(delta == 1)
-
-print(f"Cela fait {(today-reveal_date).days} jours que Hollow Knight: Silksong a été annoncé.")
-print("Il a été annoncé que Hollow Knight: Silksong sortira le 4 septembre 2025.")
-print(f"Il reste donc {delta} jours, et Silksong sortira demain avec une probabilité de {probability}.")
+#print(f"Cela fait {(today-reveal_date).days} jours que Hollow Knight: Silksong a été annoncé.")
+#print("Il a été annoncé que Hollow Knight: Silksong sortira le 4 septembre 2025.")
+#print(f"Il reste donc {delta} jours, et Silksong sortira demain avec une probabilité de {probability}.")
