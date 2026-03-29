16945be8ed2b7ebc2cc41e4be0b17d7bd0c138b6
Jean Dubois <jd-dev@laposte.net>
2026-03-29 12:22:13 +0100

    forgot to commit that

--- python/mess_and_misc/small_experiments/philo.py
+++ python/mess_and_misc/small_experiments/philo.py
@@ -3,8 +3,8 @@
 import random
 
 
-def generate_sujet():
-    start = [
+def generate_sujet() -> str:
+    start: list[tuple[str, int, list[str]]] = [
         ("conscience", 1, ["de soi"]),
         ("temps", 0, ["pour soi"]),
         ("liberté", 1, ["de penser", "d’opinion", "d’agir"]),
@@ -21,15 +21,25 @@ def generate_sujet():
         ("état d’esprit", False),
         ("selon vous assimilable", None)
     ]
+    end2 = [
+        ("être considéré comme", "être considérée comme"),
+        ("être", "être")
+    ]
     adjective = [
         ("considéré", "considérée", ["comme une énigme", "comme une réflexion", "de façon métaphysique"]),
-        ("en tant que", "en tant que", ["valeur"]),
+        ("en tant que", "en tant que", ["valeur", "notion fondamentale"]),
+        ("", "", [""]),
+        ("", "", [""]),
         ("", "", [""])
     ]
     s = start.pop(random.randint(0, len(start)-1))
-    e1 = random.choice(end1)
-    e2 = random.choice(start)
-    e3 = random.choice(adjective)
+    whichend = random.randint(1, 2)
+    if whichend == 1:
+        e1 = random.choice(end1)
+    else:
+        e1 = random.choice(end2)
+    randomend = random.choice(start)
+    randomadj = random.choice(adjective)
     if s[1] == 0:
         result = "Le "
     elif s[1] == 1:
@@ -41,25 +51,41 @@ def generate_sujet():
         if random.randint(0, 1) == 0:
             result += random.choice(s[2]) + " "
 
-    if s[1] == 0 or s[1] == 2:
-        result += "est-il "
-    elif s[1] == 1 or s[1] == 3:
-        result += "est-elle "
-
-    if e1[1] is None:
-        pass
-    elif e1[1]:
-        result += "une "
+    if whichend == 1:
+        if s[1] == 0 or s[1] == 2:
+            result += "est-il "
+        elif s[1] == 1 or s[1] == 3:
+            result += "est-elle "
     else:
-        result += "un "
-    result += e1[0] + " "
+        if s[1] == 0 or s[1] == 2:
+            result += "peut-il "
+        elif s[1] == 1 or s[1] == 3:
+            result += "peut-elle "
 
-    if e2[1] == 0:
+    if random.randint(0, 5) == 0:
+        result += "toujours "
+
+    if whichend == 1:
+        if e1[1] is None:
+            pass
+        elif e1[1]:
+            result += "une "
+        else:
+            result += "un "
+        result += e1[0] + " "
+    else:
+        if s[1] == 0 or s[1] == 2:
+            result += e1[0] + " "
+        else:
+            result += e1[1] + " "
+
+
+    if randomend[1] == 0:
         if e1[1] is None:
             result += "au "
         else:
             result += "du "
-    elif e2[1] == 1:
+    elif randomend[1] == 1:
         if e1[1] is None:
             result += "à la "
         else:
@@ -69,14 +95,14 @@ def generate_sujet():
             result += "à l’"
         else:
             result += "de l’"
-    result += e2[0] + " "
-    if len(e2[2]) != 0:
+    result += randomend[0] + " "
+    if len(randomend[2]) != 0:
         if random.randint(0, 1) == 0:
-            result += random.choice(e2[2]) + " "
+            result += random.choice(randomend[2]) + " "
 
-    index_in_e3 = int(e2[1])%2
-    result += e3[index_in_e3] + " "
-    result += random.choice(e3[2])
+    index_in_e3 = int(randomend[1])%2
+    result += randomadj[index_in_e3] + " "
+    result += random.choice(randomadj[2])
     while result.endswith(" "):
         result = result[:-1]
     result += "\N{NBSP}?"
